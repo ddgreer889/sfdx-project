d30bb95b9d44917c708edd5752b1ecaf076485c5
Oscar <oscarsu18@gmail.com>
2019-03-15 15:55:49 -0400

    clear files

--- .sfdx/typings/lwc/apex/afPTOTabsApexController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/afPTOTabsApexController.getAllPTOs" {
-  export default function getAllPTOs(param: {user: any}): Promise<any>;
-}
-declare module "@salesforce/apex/afPTOTabsApexController.getBatches" {
-  export default function getBatches(param: {user: any}): Promise<any>;
-}
--- .sfdx/typings/lwc/apex/afTrainersTabPTOApexController.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/afTrainersTabPTOApexController.getTrainingPTOById" {
-  export default function getTrainingPTOById(param: {userId: any}): Promise<any>;
-}
