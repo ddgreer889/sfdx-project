7be32e7923cd4d85403b695e549f64bb416c38f8
Oscar <oscarsu18@gmail.com>
2019-03-15 19:19:33 -0400

    Test

--- force-app/main/default/aura/afTrainersTabBatches/afTrainersTabBatchesController.js
+++ force-app/main/default/aura/afTrainersTabBatches/afTrainersTabBatchesController.js
@@ -3,7 +3,7 @@
    TrainersBatches : function(component, event, helper) {
        component.set('v.empBatchesRecordset', [
 
-           { label: 'Start Date', fieldName: 'startDate', type: 'date'  },
+           { label: 'Start Date', fieldName: 'startDate', type: 'date', initialWidth:'30px'  },
            { label: 'End Date', fieldName: 'endDate', type: 'date'  },
            { label: 'Status', fieldName: 'status', type: 'text', typeAttribute: {type: 'helpers.randomize', values: ['Planned', 'Confirmed', 'In Progress', 'Completed', 'Cancelled']}},
            { label: 'Track', fieldName: 'track', type: 'text', },
