10f84fd46ae4022d8ecb753ad8d54574ba01a0b8
Oscar <oscarsu18@gmail.com>
2019-03-15 19:24:41 -0400

    first class merge

--- force-app/main/default/aura/afTrainersTabBatches/afTrainersTabBatchesController.js
+++ force-app/main/default/aura/afTrainersTabBatches/afTrainersTabBatchesController.js
@@ -1,9 +1,10 @@
+<<<<<<< HEAD
 ({
    //Initilizes inital chart values and calls helper function getData to do server call back
    TrainersBatches : function(component, event, helper) {
        component.set('v.empBatchesRecordset', [
 
-           { label: 'Start Date', fieldName: 'startDate', type: 'date', initialWidth:'30px'  },
+           { label: 'Start Date', fieldName: 'startDate', type: 'date'  },
            { label: 'End Date', fieldName: 'endDate', type: 'date'  },
            { label: 'Status', fieldName: 'status', type: 'text', typeAttribute: {type: 'helpers.randomize', values: ['Planned', 'Confirmed', 'In Progress', 'Completed', 'Cancelled']}},
            { label: 'Track', fieldName: 'track', type: 'text', },
@@ -11,4 +12,19 @@
        ]);
            helper.getData(component, event);
            },
+=======
+({
+   //Initilizes inital chart values and calls helper function getData to do server call back
+   TrainersBatches : function(component, event, helper) {
+       component.set('v.empBatchesRecordset', [
+
+           { label: 'Start Date', fieldName: 'startDate', type: 'date'  },
+           { label: 'End Date', fieldName: 'endDate', type: 'date'  },
+           { label: 'Status', fieldName: 'status', type: 'text', typeAttribute: {type: 'helpers.randomize', values: ['Planned', 'Confirmed', 'In Progress', 'Completed', 'Cancelled']}},
+           { label: 'Track', fieldName: 'track', type: 'text', },
+
+       ]);
+           helper.getData(component, event);
+           },
+>>>>>>> 3fb98fe170202194ecee298609c3303f56548c4b
 })
