4bf92bb0321ab1a55bdab75239b341d202e1bdc0
Nathaniel Jaime <nejaime97@gmail.com>
2019-03-15 19:25:03 -0400

    Find Class Merge

--- force-app/main/default/aura/afPTOTabs/afPTOTabsHelper.js
+++ force-app/main/default/aura/afPTOTabs/afPTOTabsHelper.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ({
     sortPTOs : function(List) {
         return List.sort(function compare(a, b) {
@@ -6,4 +7,14 @@
             return dateA - dateB;
         });
     }
+=======
+({
+    sortPTOs : function(List) {
+        return List.sort(function compare(a, b) {
+            var dateA = new Date(a.StartDate__c);
+            var dateB = new Date(b.StartDate__c);
+            return dateA - dateB;
+        });
+    }
+>>>>>>> 3fb98fe170202194ecee298609c3303f56548c4b
 })
