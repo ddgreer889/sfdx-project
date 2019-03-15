94a50e9ddddca566dbdb404fb42c6d377582b52d
iblanc231 <iblanc231@gmail.com>
2019-03-15 19:13:55 -0400

    Final Merge into Testing

--- force-app/main/default/aura/afNewPTO/afNewPTOHelper.js
+++ force-app/main/default/aura/afNewPTO/afNewPTOHelper.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ({
 	clear : function(component, event) {   
         
@@ -7,4 +8,15 @@
         component.set("v.endDate",null);
         component.set("v.reason","");
     }
+=======
+({
+	clear : function(component, event) {   
+        
+        component.set("v.uncleared", false);
+        component.set("v.uncleared", true);
+        component.set("v.startDate",null);
+        component.set("v.endDate",null);
+        component.set("v.reason","");
+    }
+>>>>>>> 3fb98fe170202194ecee298609c3303f56548c4b
 })
