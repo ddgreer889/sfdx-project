a871fdae557b615e60ddf673eaa5403cfdaebab8
s3rj <astralninja@gmail.com>
2019-03-15 19:24:30 -0400

    first class merge

--- force-app/main/default/aura/afTrainerAvailabilityListItem/afTrainerAvailabilityListItemController.js
+++ force-app/main/default/aura/afTrainerAvailabilityListItem/afTrainerAvailabilityListItemController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ({
     doInit : function(component, event, helper) {
         var availability = component.get('v.availability');
@@ -16,4 +17,24 @@
         selectedEvt.setParams({'trainerId':trainerId});
         selectedEvt.fire();
     },
+=======
+({
+    doInit : function(component, event, helper) {
+        var availability = component.get('v.availability');
+        var trainerId = component.get('v.trainerId');
+        if(availability==="Available"){
+            component.set('v.isAvailable', true);
+        } else{
+            component.set('v.isAvailable', false);
+        }
+        
+    },
+    selectIsClicked : function(component, event, helper){
+        var selectedEvt = $A.get('e.c:TrainerSelected');
+        var trainerId = component.get('v.trainerId');
+        console.log(trainerId);
+        selectedEvt.setParams({'trainerId':trainerId});
+        selectedEvt.fire();
+    },
+>>>>>>> 3fb98fe170202194ecee298609c3303f56548c4b
 })
--- force-app/main/default/aura/afTrainersTabPTO/afTrainersTabPTOController.js
+++ force-app/main/default/aura/afTrainersTabPTO/afTrainersTabPTOController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ({
    TrainersPTO : function (component, event, helper){
        component.set('v.empPTORecordset', [
@@ -10,4 +11,18 @@
        ]);
        helper.getData(component, event);
    },
+=======
+({
+   TrainersPTO : function (component, event, helper){
+       component.set('v.empPTORecordset', [
+
+           { label: 'Start Date', fieldName: 'startDate', type: 'date', sortable: 'true' },
+           { label: 'End Date', fieldName: 'endDate', type: 'date', sortable: 'true' },
+           { label: 'Status', fieldName: 'status', type: 'text', sortable: 'true'},
+           { label: 'Reason', fieldName: 'reason', type: 'text', sortable: 'true'},
+
+       ]);
+       helper.getData(component, event);
+   },
+>>>>>>> 3fb98fe170202194ecee298609c3303f56548c4b
 })
