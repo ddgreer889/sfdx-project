ae0fe69b753ea9af4d6c6db0c6aef44c20a31f80
Evan Strohm <evanstrohm@gmail.com>
2019-03-15 20:19:17 -0400

    First REAL class merge?

--- force-app/main/default/aura/afPTOTabs/afPTOTabsHelper.js
+++ force-app/main/default/aura/afPTOTabs/afPTOTabsHelper.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ({
     sortPTOs : function(List) {
         return List.sort(function compare(a, b) {
@@ -6,15 +5,17 @@
             var dateB = new Date(b.StartDate__c);
             return dateA - dateB;
         });
+    },
+
+    //Gets the component's Lists of Pending/Approved PTOs and sets the hasPending/hasApproved attribute accordingly
+    setPTOTabs : function(component) {
+        var ptos = component.get('v.pendingPTOs');
+        component.set('v.hasPending', ptos.length != 0);
+        
+        ptos = component.get('v.approvedPTOs');
+        var newptos = component.get('v.newApprovedPTOs');
+        var oldptos = component.get('v.oldApprovedPTOs');
+        component.set('v.hasApproved', ptos.length != 0 || 
+                        oldptos.length != 0 || newptos.length != 0);
     }
-=======
-({
-    sortPTOs : function(List) {
-        return List.sort(function compare(a, b) {
-            var dateA = new Date(a.StartDate__c);
-            var dateB = new Date(b.StartDate__c);
-            return dateA - dateB;
-        });
-    }
->>>>>>> 3fb98fe170202194ecee298609c3303f56548c4b
 })
