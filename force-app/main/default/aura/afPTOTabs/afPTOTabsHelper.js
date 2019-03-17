9eb130fd868572581824f0978b08282b9cd1ffba
Nathaniel Jaime <nejaime97@gmail.com>
2019-03-17 11:33:33 -0400

    Post Deployment Commit

--- force-app/main/default/aura/afPTOTabs/afPTOTabsHelper.js
+++ force-app/main/default/aura/afPTOTabs/afPTOTabsHelper.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ({
     sortPTOs : function(List) {
         return List.sort(function compare(a, b) {
@@ -7,14 +6,4 @@
             return dateA - dateB;
         });
     }
-=======
-({
-    sortPTOs : function(List) {
-        return List.sort(function compare(a, b) {
-            var dateA = new Date(a.StartDate__c);
-            var dateB = new Date(b.StartDate__c);
-            return dateA - dateB;
-        });
-    }
->>>>>>> 3fb98fe170202194ecee298609c3303f56548c4b
 })
