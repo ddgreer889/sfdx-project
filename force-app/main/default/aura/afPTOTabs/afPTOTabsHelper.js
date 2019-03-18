3bc0a47daa8fceac509251437d56d69386497af7
Sharif <selkasse@gmu.edu>
2019-03-18 16:32:23 -0400

    retrieved aura folder

--- force-app/main/default/aura/afPTOTabs/afPTOTabsHelper.js
+++ force-app/main/default/aura/afPTOTabs/afPTOTabsHelper.js
@@ -5,17 +5,5 @@
             var dateB = new Date(b.StartDate__c);
             return dateA - dateB;
         });
-    },
-
-    //Gets the component's Lists of Pending/Approved PTOs and sets the hasPending/hasApproved attribute accordingly
-    setPTOTabs : function(component) {
-        var ptos = component.get('v.pendingPTOs');
-        component.set('v.hasPending', ptos.length != 0);
-        
-        ptos = component.get('v.approvedPTOs');
-        var newptos = component.get('v.newApprovedPTOs');
-        var oldptos = component.get('v.oldApprovedPTOs');
-        component.set('v.hasApproved', ptos.length != 0 || 
-                        oldptos.length != 0 || newptos.length != 0);
     }
 })
