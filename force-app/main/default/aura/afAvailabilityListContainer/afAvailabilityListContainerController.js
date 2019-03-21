30c0f60adbb01b4edcd0ab5f572afbf0c879add1
Robert Chhoeut <rchhoeut@gmail.com>
2019-03-21 18:19:55 -0400

    afAvailabilityList moved logic for events of room and training track changed to server side

--- force-app/main/default/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
+++ force-app/main/default/aura/afAvailabilityListContainer/afAvailabilityListContainerController.js
@@ -1,63 +1,41 @@
 ({
-    doInit : function(component, event, helper) {
-        var getSkills = component.get("c.getAllSkills");
-        //sets a call back from the server to get an array of all the skills records
-        getSkills.setCallback(this, function(response){
-            var state = response.getState();
-            if(component.isValid() && state === "SUCCESS"){
-                component.set('v.allSkills', response.getReturnValue());
-            } else{
-                console.log('Error1');
-            }
-        });
-        $A.enqueueAction(getSkills);
-        var getTrainings = component.get("c.getAllTrainings");
-        //sets a call back from the server to get an array of all the training records
-        getTrainings.setCallback(this, function(response){
-            var state = response.getState();
-            if(component.isValid() && state === "SUCCESS"){
-                component.set('v.allTrainings', response.getReturnValue());
-            } else{
-                console.log('Error2');
-            }
-        });
-        $A.enqueueAction(getTrainings);
-        var action = component.get("c.getAllTrainers");
-        //sets a call back from the server to get an array of users with the trainer role
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if(component.isValid() && state === "SUCCESS"){
-                var trainers = helper.resetHasSkill(response.getReturnValue());
-                // console.log('trainers' + trainers);
-                component.set('v.trainers', helper.sortTrainers(trainers));
-            } else{
-                console.log('Error3');
-            }
-        });
-        $A.enqueueAction(action);
-        
+    initRooms : function(component, event, helper) {                
         var getRooms = component.get("c.getRooms");
+        getRooms.setParams({ allRooms : component.get("v.allRooms") });
         //sets a call back from the server to get an array of all the training room records
         getRooms.setCallback(this, function(response) {
-            var state = response.getState();
-            
-            if(component.isValid && state === 'SUCCESS'){                
-                component.set("v.rooms", helper.sortTrainingRoom(response.getReturnValue()));
-                component.set("v.currentLocRooms", helper.sortTrainingRoom(response.getReturnValue()));
+            var state = response.getState();       
+            if(component.isValid && state === 'SUCCESS'){  
+                component.set("v.rooms", response.getReturnValue());
+                component.set("v.currentLocRooms", response.getReturnValue());
             }else if(state === 'ERROR'){
-                var errors = response.getError();
-                
+                var errors = response.getError();               
                 if(errors){
                     if(errors[0] && errors[0].message){
                         console.log('Error message: ' + errors[0].message);
+                        alert('Error message: ' + errors[0].message);
                     }
                 }
             }else{
                 console.log('Unknown error');
             }
         });
-        $A.enqueueAction(getRooms);
-        
+        $A.enqueueAction(getRooms); 
+    },
+    
+    initTrainers : function(component, event, helper){
+        var action = component.get("c.getAllTrainers");
+        action.setParams({ Trainers : component.get("v.allTrainers") });
+        //sets a call back from the server to get an array of users with the trainer role
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(component.isValid() && state === "SUCCESS"){
+                component.set('v.trainers', response.getReturnValue());
+            } else{
+                console.log('Error3');
+            }
+        });
+        $A.enqueueAction(action);    
     },
     
     dateHasChanged: function(component, event, helper){
@@ -67,9 +45,12 @@
         var startDate = new Date(event.getParam('startDate'));
         var endDate = new Date(event.getParam('endDate'));
         var currentLocRooms = component.get('v.currentLocRooms');
+        
+        
         for(var i=0; i<trainers.length; i++){
             for (var j = 0; j < trainings.length; j++) {
-                if(trainers[i].Id == trainings[j].Trainer__c || trainers[i].Id == trainings[j].CoTrainer__c) {
+                if(trainers[i].Id == trainings[j].Trainer__c || 
+                   trainers[i].Id == trainings[j].CoTrainer__c) {
                     var prevStart = new Date(trainings[j].StartDate__c);
                     //prevStart date is a variable to store the Date value of the training start date
                     var prevEnd = new Date(trainings[j].EndDate__c);
@@ -86,7 +67,7 @@
                 }
             }
         }
-         for(var i=0; i<currentLocRooms.length; i++){
+        for(var i=0; i<currentLocRooms.length; i++){
             for (var j = 0; j < trainings.length; j++) {
                 if(currentLocRooms[i].Id == trainings[j].TrainingRoom__c) {
                     var prevStart = new Date(trainings[j].StartDate__c);
@@ -112,23 +93,25 @@
     },
     
     handleLoc : function(component, event, helper) {
-        var loc = event.getParam("location");
-        component.set("v.location", loc);
-        var currentLocRooms = [];
-        var rooms = component.get("v.rooms");
-        
-        for(var i = 0; i < rooms.length; i++){
-            if(rooms[i].TrainingLocation__c == loc){
-                currentLocRooms.push(rooms[i]);
-            }        
-        }
-        if(loc == "" || null){
-            component.set("v.currentLocRooms", null);
-            component.set("v.currentLocRooms", helper.sortTrainingRoom(rooms));
-        } else {
-            component.set("v.currentLocRooms", null);
-            component.set("v.currentLocRooms", helper.sortTrainingRoom(currentLocRooms));
-        } 
+        var getNewRooms = component.get("c.RoomsMatchLocation");
+        getNewRooms.setParams({location : event.getParam("location"),trl : component.get("v.rooms")});
+        getNewRooms.setCallback(this, function(response){
+            var state = response.getState();
+            if(component.isValid && state === 'SUCCESS'){                
+                component.set("v.currentLocRooms", response.getReturnValue());
+            }else if(state === 'ERROR'){
+                var errors = response.getError();
+                
+                if(errors){
+                    if(errors[0] && errors[0].message){
+                        console.log('Error message: ' + errors[0].message);
+                    }
+                }
+            }else{
+                console.log('Unknown error');
+            }
+        });
+        $A.enqueueAction(getNewRooms);
     },
     
     roomClick: function(component, helper){
@@ -140,14 +123,40 @@
     },
     
     skillHasChanged: function(component, event, helper){
-        //this method handles the event to send the new selected training track to all of the trainers
-        var trainers = component.get('v.trainers');
-        var trainingTrack = event.getParam('track');
-        var skills = component.get('v.allSkills');
-        component.set('v.selectedTrainingTrack',trainingTrack);
-        trainers = helper.checkHasSkill(trainers, skills, trainingTrack);
-        component.set('v.trainers', null);
-        component.set('v.trainers', helper.sortTrainers(trainers));
+        var trainingTrack = event.getParam('track').toString()
+        if(trainingTrack==""||null){
+            trainingTrack = null;
+        }
+        
+        var trainerHasSkill = component.get("c.TrainerHasSkill");
+        trainerHasSkill.setParams({ 
+            						"track" : trainingTrack,
+                                    "trainers" : component.get('v.trainers'),
+                                    "skills" : component.get('v.allSkills')
+                                  });
+        //sets a call back from the server to get an array of all the trainers that have or don't have the skill
+        trainerHasSkill.setCallback(this, function(response) {
+            var state = response.getState();
+            
+            if(component.isValid && state === 'SUCCESS'){  
+                //right now, it is using the helper sort trainers
+                //whomever is doing the apex logic to sort trainers by hasSkill,
+                //call that method within the apex and remove the helper.sortTrainers below
+                component.set('v.trainers', helper.sortTrainers(response.getReturnValue() ));
+            }else if(state === 'ERROR'){
+                var errors = response.getError();               
+                if(errors){
+                    if(errors[0] && errors[0].message){
+                        console.log('Error message: ' + errors[0].message);
+                        alert('Error message: ' + errors[0].message);
+                    }
+                }
+            }else{
+                console.log('Unknown error');
+            }
+        });
+        $A.enqueueAction(trainerHasSkill);         
+        
     },
     
     trainerClick: function (component, helper) {
@@ -157,18 +166,4 @@
             component.set('v.tab1Shown', true);
         }
     },
-    
-    /*trainingTrackCreated: function(component, event, helper){
-        var trainings = component.get('v.allTrainings');
-        var trainers = component.get('v.trainers');
-        var currentLocRooms = component.get('v.currentLocRooms');
-        var newTraining = event.getParam('newBatch');
-        trainings.push(newTraining);
-        component.set('allTrainings', trainings);
-        component.set('v.trainers', null);
-        component.set('v.trainers', helper.sortTrainers(helper.sortTrainers(trainers)));
-        component.set('v.currentLocRooms', null);
-        component.set('v.currentLocRooms', currentLocRooms);
-        
-    },*/
 })
---
+++ force-app/main/default/aura/afNewBatchAndAvailabilityComponents/afNewBatchAndAvailabilityComponentsController.js
@@ -0,0 +1,83 @@
+({
+	doInit : function(component, event, helper) {
+        //Get all locations
+        var allLocs = [];
+        var locAction = component.get("c.masterLocations");      
+        locAction.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                allLocs = response.getReturnValue();
+                allLocs.unshift(null);
+                component.set("v.allLocations", allLocs);   
+                
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log('Error message: ' + errors[0].message);
+                    }
+                }
+            } else {
+                console.log('Unknown error.')
+            }
+        })
+        $A.enqueueAction(locAction);
+        
+        //Get all Rooms
+        var roomAction = component.get("c.masterRooms");       
+        roomAction.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.allRooms", response.getReturnValue());   
+                //alert(component.get("v.allRooms"));
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log('Error message: ' + errors[0].message)
+                    }
+                }
+            } else {
+                console.log('Unknown error.')
+            }
+        })
+        $A.enqueueAction(roomAction);
+        
+        //Get all Skills
+        var getSkills = component.get("c.masterSkills");
+        getSkills.setCallback(this, function(response) {
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                component.set('v.allSkills', response.getReturnValue());
+            } else {
+                console.log('Error1');
+            }
+        });
+        $A.enqueueAction(getSkills);
+        
+        //Get all Trainers
+        var action = component.get("c.masterTrainers");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                component.set('v.allTrainers', response.getReturnValue());
+            } else {
+                console.log('Error3');
+            }
+        });
+        $A.enqueueAction(action);
+        
+        //Get all Trainings
+        var getTrainings = component.get("c.masterTrainings");
+        getTrainings.setCallback(this, function(response) {
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                component.set('v.allTrainings', response.getReturnValue());                
+            } else {
+                console.log('Error2');
+            }
+        });
+        $A.enqueueAction(getTrainings);
+		        
+	}
+})
