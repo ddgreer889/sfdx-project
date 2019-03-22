e945adaa4b9de8233f45f01d078692a69c7676e6
JD <JDP.SEProductions@gmail.com>
2019-03-21 22:30:33 -0400

    Final Changes to Collective Merge. Changes to PTO, Batch Tab, User Profile, Trainings, Batch Timeline.

--- force-app/main/default/aura/afNewBatchForm/afNewBatchFormHelper.js
+++ force-app/main/default/aura/afNewBatchForm/afNewBatchFormHelper.js
@@ -17,7 +17,6 @@
             }
             // convert to legible date format
             let year = endDate.getUTCFullYear(); 
-<<<<<<< HEAD
             let month = endDate.getUTCMonth();
             let date = endDate.getUTCDate();
 
@@ -25,10 +24,6 @@
             if(endDate.getUTCDay() === 4){
                 date += 1;
             }
-=======
-            let month = endDate.getUTCMonth(); 
-            let date = endDate.getUTCDate();
->>>>>>> origin/Batch-Robert
             
             component.set("v.endDate", (year + "-" + (month+1) + "-" + date));
             
